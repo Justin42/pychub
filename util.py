67b4689e0cd0f4ed4d0dac4b5546d57057dbb2a2
Justin Baldwin <justin.d.baldwin91@gmail.com>
2015-11-03 05:22:47 -0500

    Added utility for converting character dict to database object

--- util.py
+++ util.py
@@ -1,4 +1,8 @@
-from pychub import FreeCompany
+from mongoengine import DoesNotExist
+
+from model import classes
+from model.User import Character, ClassData, AchievementInfo
+from model.FreeCompany import FreeCompany
 from pychub.model.FreeCompany import FreeCompanyEstate
 
 
@@ -14,3 +18,26 @@ def fc_from_dict(fc_dict):
             estate.__setattr__(key, value)
         fc.estate = estate
     return fc
+
+
+def character_from_dict(char_dict):
+    character = Character()
+
+    for key, value in char_dict.items():
+        setattr(character, key, value)
+
+    character.classes = [
+        ClassData(name=c['name'], level=c['level'], current_exp=c['current_exp'], next_exp=c['next_exp']) for
+        c in char_dict['classes']
+    ]
+
+    character.recent_achievements = [
+        AchievementInfo(date=a['date'], type=a['type'], name=a['name'], text=a['text']) for
+        a in char_dict['recent_achievements']
+    ]
+
+    try:
+        character.free_company = FreeCompany.objects.get(lodestone_id=char_dict['free_company'])
+    except DoesNotExist:  # TODO Logging
+        return None
+    return character
