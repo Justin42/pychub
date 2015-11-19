fd695091a6b1dcc6688f5f828c0c7ced04fe3d9d
Justin Baldwin <justin.d.baldwin91@gmail.com>
2015-11-19 01:11:17 -0500

    Refactoring

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'transaction',
+    'waitress', 'mongoengine', 'bcrypt'
+    ]
+
+setup(name='PyChub',
+      version='0.0',
+      description='PyChub',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='pychub',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = pychub:main
+      [console_scripts]
+      initialize_PyChub_db = pychub.scripts.initializedb:main
+      """,
+      )
